0eef6a3dd3ebe76b8ee0ed011827939a293bc938
hkabesa <haim@creator.eco>
2019-12-31 14:58:33 +0200

    using env vars for pinata api

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 const pinataSDK = require('@pinata/sdk');
-const pinata = pinataSDK('process.env.PINATA_API_KEY', 'process.env.PINATA_SECRET_API_KEY');
+const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_API_KEY);
 
 pinata.testAuthentication().then((result) => {
     //handle successful authentication here

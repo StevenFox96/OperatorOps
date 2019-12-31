f78c642e72cca250479377bf7a196489f89837e3
hkabesa <haim@creator.eco>
2019-12-31 17:07:38 +0200

    update workflow

---
+++ frontend-deployer/app.js
@@ -0,0 +1,22 @@
+const pinataSDK = require('@pinata/sdk');
+const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_API_KEY);
+const sourcePath = '/Users/haimkabesa/work/code/github/CreatorEco/OperatorOps/frontend-deployer/frontend/src';
+const options = {
+    pinataMetadata: {
+        name: 'My Awesome Website',
+        keyvalues: {
+            customKey: 'customValue',
+            customKey2: 'customValue2'
+        }
+    },
+    pinataOptions: {
+        cidVersion: 0
+    }
+};
+pinata.pinFromFS(sourcePath, options).then((result) => {
+    //handle results here
+    console.log(result.IpfsHash);
+}).catch((err) => {
+    //handle error here
+    console.log(err);
+});

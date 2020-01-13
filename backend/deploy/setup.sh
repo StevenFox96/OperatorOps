eba54bb2d024e3d3e249b05f816e554b530aa0dd
hkabesa <haim@creator.eco>
2020-01-13 13:03:17 +0200

    update setup script

--- backend/deploy/setup.sh
+++ backend/deploy/setup.sh
@@ -12,7 +12,7 @@ source "$THIS_DIR/toolog.lib.sh" operator-setup
 
 # This are the env vars we need (defaults for test info on Kylin)
 #
-OPERATOR_ACCOUNT_NAME=${OPERATOR_ACCOUNT_NAME:-"dappletokens"}
+OPERATOR_ACCOUNT_NAME=${OPERATOR_ACCOUNT_NAME:-""}
 OPERATOR_PRIVATE_KEY=${OPERATOR_PRIVATE_KEY:-""}
 OPERATOR_CHAIN_ID=${OPERATOR_CHAIN_ID:-"5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191"}
 OPERATOR_CHAIN_NODE=${OPERATOR_CHAIN_NODE:-"https://kylin-dsp-1.liquidapps.io"}
@@ -22,8 +22,9 @@ OPERATOR_RESERVE_RELAY=${OPERATOR_RESERVE_RELAY:-"usdb2eosrely"}
 OPERATOR_RESERVE_SYMBOL=${OPERATOR_RESERVE_SYMBOL:-"USDB"}
 OPERATOR_RESERVE_RELAY_SYMBOL=${OPERATOR_RESERVE_RELAY_SYMBOL:-"USDBEOS"}
 
-CONTRACT_WASM_FILE=${CONTRACT_WASM_FILE:-"../contracts/Operator.wasm"}
-CONTRACT_ABI_FILE=${CONTRACT_ABI_FILE:-"../contracts/Operator.abi"}
+CONTRACTS_DIR=${CONTRACTS_DIR:-"../ceo-core"}
+CONTRACT_WASM_FILE=${CONTRACT_WASM_FILE:-"ceo-core.wasm"}
+CONTRACT_ABI_FILE=${CONTRACT_ABI_FILE:-"ceo-core.abi"}
 
 VACCOUNTS_PROVIDER=${VACCOUNTS_PROVIDER:-"heliosselene"}
 VACCOUNTS_SERVICE=${VACCOUNTS_SERVICE:-"accountless1"}
@@ -207,18 +208,18 @@ function create_temp_wallet() {
 function deploy() {
     loginfo "Deploying contract"
 
-    if [ ! -f "$THIS_DIR/$CONTRACT_ABI_FILE" ]
+    if [ ! -f "$CONTRACTS_DIR/$CONTRACT_ABI_FILE" ]
     then
         logerror " -> Could not find ABI file (quitting)\n"
         exit 1
-    elif [ ! -f "$THIS_DIR/$CONTRACT_WASM_FILE" ]
+    elif [ ! -f "$CONTRACTS_DIR/$CONTRACT_WASM_FILE" ]
     then
         logerror " -> Could not find WASM file (quitting)\n"
         exit 1
     fi
 
 
-    LAST_RESULT=$(cleos -u $OPERATOR_CHAIN_NODE set contract $OPERATOR_ACCOUNT_NAME $THIS_DIR -p $OPERATOR_ACCOUNT_NAME 2>&1)
+    LAST_RESULT=$(cleos -u $OPERATOR_CHAIN_NODE set contract $OPERATOR_ACCOUNT_NAME $CONTRACTS_DIR -p $OPERATOR_ACCOUNT_NAME 2>&1)
     if [ $? -ne 0 ]
     then
         logerror " -> Could not deploy contract"
